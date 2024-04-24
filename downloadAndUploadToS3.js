5e197a4cc3bc1e16afb3015b2a2533647be601c0
ashishyadav369 <84495458+ashishyadav369@users.noreply.github.com>
2024-04-24 23:26:42 +0530

    added exception handeling & updated progress bar

--- downloadAndUploadToS3.js
+++ downloadAndUploadToS3.js
@@ -3,6 +3,7 @@ const AWS = require("aws-sdk");
 const axios = require("axios");
 const { PassThrough } = require("stream");
 const cliProgress = require("cli-progress");
+const _colors = require("ansi-colors");
 
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -13,41 +14,52 @@ AWS.config.update({
 const s3 = new AWS.S3();
 const bucketName = process.env.S3_BUCKET_NAME;
 
-async function uploadVideoToS3(url, bucketName, keyName) {
-  let ar = await webexScript.genrateDownloadUrl();
-  try {
-    const response = await axios.get(ar[0].downloadUrl, {
-      responseType: "stream",
-    });
+async function uploadVideoToS3() {
+  let recordingsWithDirectUrl = await webexScript.genrateDownloadUrl();
 
-    const passThroughStream = new PassThrough();
+  for (const recording of recordingsWithDirectUrl) {
+    try {
+      const response = await axios.get(recording.downloadUrl, {
+        responseType: "stream",
+      });
 
-    response.data.pipe(passThroughStream);
-    const contentLength = parseInt(response.headers["content-length"], 10);
-    const progressBar = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-    progressBar.start(contentLength, 0);
+      const passThroughStream = new PassThrough();
 
-    const params = {
-      Bucket: bucketName,
-      Key: "huihuihuihueeewwsdfrrrrwrerwew44i",
-      Body: passThroughStream,
-    };
-    const upload = s3.upload(params);
-    upload.on("httpUploadProgress", function (event) {
-      const progress = Math.round((event.loaded / event.total) * 100);
-      progressBar.update(event.loaded);
-    });
+      response.data.pipe(passThroughStream);
+      const contentLength = parseInt(response.headers["content-length"], 10);
+      const progressBar = new cliProgress.Bar(
+        {
+          format:
+            `${recording.name} Upload Progress |` +
+            _colors.cyan("{bar}") +
+            "| {percentage}% || ETA : {eta_formatted} || {value}/{total} Bytes || {duration_formatted}",
+          barCompleteChar: "\u2588",
+          barIncompleteChar: "\u2591",
+          hideCursor: true,
+        },
+        cliProgress.Presets.shades_classic
+      );
 
-    await upload.promise();
+      progressBar.start(contentLength, 0);
 
-    progressBar.stop();
-    console.log("Video uploaded to S3:", params.Key);
-  } catch (error) {
-    console.error("Error uploading video to S3:", error);
+      const params = {
+        Bucket: bucketName,
+        Key: recording.name,
+        Body: passThroughStream,
+      };
+      const upload = s3.upload(params);
+      upload.on("httpUploadProgress", function (event) {
+        progressBar.update(event.loaded);
+      });
+
+      await upload.promise();
+
+      progressBar.stop();
+      console.log("Video uploaded to S3:", params.Key);
+    } catch (error) {
+      console.error(`Error uploading ${recording.name} to S3:`, error);
+    }
   }
 }
 
-uploadVideoToS3("", bucketName, " keyName");
+uploadVideoToS3();
--- webexScript.js
+++ webexScript.js
@@ -1,198 +1,216 @@
 require("dotenv").config();
 
 async function getRecordingsList() {
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-    },
-  };
-
-  const recordings = await fetch(
-    "https://webexapis.com/v1/recordings",
-    options
-  );
-
-  const recordingList = await recordings.json();
-  console.log(`Total recordings found : ${recordingList.items?.length}`);
-  return recordingList.items;
-}
-
-// TODO:: Improve code & cleanup
-async function genrateDownloadUrl() {
-  const recordingsWithDirectUrl = [];
-  const recordingsList = await getRecordingsList();
-  for (let i = 0; i < 1; i++) {
-    console.log(
-      `Genrating direct download url for :  ${recordingsList[i].topic}`
-    );
-
-    const resp = await fetch(recordingsList[i].downloadUrl);
-    const parsedResponse = await resp.text();
-
-    const regexForRecordId =
-      /<input\s+type="hidden"\s+name="recordID"\s+value='(\d+)'>/i;
-    const matchRecordId = regexForRecordId.exec(parsedResponse);
-    const recordID = matchRecordId ? matchRecordId[1] : null;
-
-    //
-    const urlObj = new URL(recordingsList[i].downloadUrl);
-    const rcid = urlObj.searchParams.get("RCID");
-
-    const myHeaders = new Headers();
-    myHeaders.append(
-      "Accept",
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-    );
-    myHeaders.append("Referer", `${recordingsList[i].downloadUrl}`);
-    myHeaders.append("Origin", `${process.env.ORIGIN}`);
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("siteurl", `${process.env.SITE_URL}`);
-    urlencoded.append("action", "dw");
-    urlencoded.append("firstEntry", "false");
-    urlencoded.append("hashRecordKey", "");
-    urlencoded.append("rcid", `${rcid}`);
-    urlencoded.append("recordID", `${recordID}`);
-    urlencoded.append("password", `${recordingsList[i].password}`);
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
+  try {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
     };
 
-    const responseAfterPasswordVerification = await fetch(
-      `${process.env.ORIGIN}/svc3300/svccomponents/servicerecordings/recordingpasswordcheck.do`,
-      requestOptions
-    );
-    const parsedresponseAfterPasswordVerification =
-      await responseAfterPasswordVerification.text();
-    const regexForDownloadUrl = /var\s+href='(.*?)';/;
-
-    const downloadUrl = regexForDownloadUrl.exec(
-      parsedresponseAfterPasswordVerification
-    )[1];
-
-    const downloadUrlObj = new URL(downloadUrl);
-    const recordKey = downloadUrlObj.searchParams.get("recordKey");
-    const recordIDDownload = downloadUrlObj.searchParams.get("recordID");
-    const serviceRecordIDDownload =
-      downloadUrlObj.searchParams.get("serviceRecordID");
-
-    const myHeadersGenrateDownload = new Headers();
-    myHeadersGenrateDownload.append(
-      "User-Agent",
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0"
-    );
-    myHeadersGenrateDownload.append(
-      "Accept",
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-    );
-    myHeadersGenrateDownload.append(
-      "Referer",
-      `${process.env.ORIGIN}/svc3300/svccomponents/servicerecordings/recordingpasswordcheck.do`
-    );
-    myHeadersGenrateDownload.append(
-      "Content-Type",
-      "application/x-www-form-urlencoded"
-    );
-    myHeadersGenrateDownload.append("Origin", `${process.env.ORIGIN}`);
-
-    const urlencodedDownload = new URLSearchParams();
-    urlencodedDownload.append("siteurl", `${process.env.SITE_URL}`);
-    urlencodedDownload.append("action", "publishfile");
-    urlencodedDownload.append("recordID", `${recordIDDownload}`);
-    urlencodedDownload.append("serviceRecordID", `${serviceRecordIDDownload}`);
-    urlencodedDownload.append("recordKey", `${recordKey}`);
-
-    const requestOptionsGenerateDownload = {
-      method: "POST",
-      headers: myHeadersGenrateDownload,
-      body: urlencodedDownload,
-      redirect: "follow",
-    };
-
-    const responseDownloadPublish = await fetch(
-      `${process.env.ORIGIN}/mw3300/mywebex/nbrshared.do`,
-      requestOptionsGenerateDownload
+    const recordings = await fetch(
+      "https://webexapis.com/v1/recordings",
+      options
     );
 
-    const parsedResponseDownloadPublish = await responseDownloadPublish.text();
-
-    const downloadUrlRegex = /downloadUrl = '(.*?)'/;
-    const recordIdRegex = /var recordId = (\d+);/;
-    const serviceRecordIdRegex = /var serviceRecordId = (\d+);/;
-    const prepareTicketRegex = /var prepareTicket = '([^']+)';/;
-
-    const downloadUrlMatch =
-      parsedResponseDownloadPublish.match(downloadUrlRegex);
-    const recordIdMatch = parsedResponseDownloadPublish.match(recordIdRegex);
-    const serviceRecordIdMatch =
-      parsedResponseDownloadPublish.match(serviceRecordIdRegex);
-    const prepareTicketMatch =
-      parsedResponseDownloadPublish.match(prepareTicketRegex);
-
-    let recordIdForDownloadUrl;
-    let serviceRecordIdForDownloadUrl;
-    let prepareTicketForDownloadUrl;
-    let baseDownloadUrl;
-    if (recordIdMatch && recordIdMatch[1]) {
-      recordIdForDownloadUrl = recordIdMatch[1];
-    }
-
-    if (serviceRecordIdMatch && serviceRecordIdMatch[1]) {
-      serviceRecordIdForDownloadUrl = serviceRecordIdMatch[1];
-    }
-
-    if (prepareTicketMatch && prepareTicketMatch[1]) {
-      prepareTicketForDownloadUrl = prepareTicketMatch[1];
-    }
-
-    if (downloadUrlMatch && downloadUrlMatch[1]) {
-      baseDownloadUrl = downloadUrlMatch[1];
-    }
-
-    const myHeadersDirectDownload = new Headers();
-    myHeadersDirectDownload.append(
-      "User-Agent",
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0"
-    );
-    myHeadersDirectDownload.append(
-      "Accept",
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-    );
-
-    myHeadersDirectDownload.append(
-      "Referer",
-      `${process.env.ORIGIN}/mw3300/mywebex/nbrshared.do`
-    );
-
-    let directDownloadResponse = await fetch(
-      `${process.env.ORIGIN}/mw3300/mywebex/nbrPrepare.do?siteurl=${process.env.SITE_URL}&recordid=${recordIdForDownloadUrl}&prepareTicket=${prepareTicketForDownloadUrl}&serviceRecordId=${serviceRecordIdForDownloadUrl}`
-    );
-
-    let parsedDirectDownloadResponse = await directDownloadResponse.text();
-
-    const regexDirectDownloadUrl =
-      /window\.parent\.func_prepare\('([^']*)','([^']*)','([^']*)'/;
-    const directDownloadUrl = parsedDirectDownloadResponse.match(
-      regexDirectDownloadUrl
-    );
-
-    const finalUrl = baseDownloadUrl + directDownloadUrl[3];
-    recordingsWithDirectUrl.push({
-      downloadUrl: finalUrl,
-      name: recordingsList[i].topic,
-    });
+    const recordingList = await recordings.json();
+    console.log(`Total recordings found : ${recordingList.items?.length}`);
+    return recordingList.items;
+  } catch (error) {
+    console.error("Error parsing recording list");
   }
-  console.log("Finished genrerating download urls for recordings");
-  return recordingsWithDirectUrl;
 }
 
-// TODO:: Exception handelling
+async function genrateDownloadUrl() {
+  try {
+    const recordingsWithDirectUrl = [];
+    const recordingsList = await getRecordingsList();
+    for (const recording of recordingsList) {
+      try {
+        console.log(`Genrating direct download url for :  ${recording.topic}`);
+
+        // Request#1
+        const resp = await fetch(recording.downloadUrl);
+        const parsedResponse = await resp.text();
+
+        const regexForRecordId =
+          /<input\s+type="hidden"\s+name="recordID"\s+value='(\d+)'>/i;
+        const matchRecordId = regexForRecordId.exec(parsedResponse);
+        const recordID = matchRecordId ? matchRecordId[1] : null;
+
+        const urlObj = new URL(recording.downloadUrl);
+        const rcid = urlObj.searchParams.get("RCID");
+
+        const myHeaders = new Headers();
+        myHeaders.append(
+          "Accept",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+        );
+        myHeaders.append("Referer", `${recording.downloadUrl}`);
+        myHeaders.append("Origin", `${process.env.ORIGIN}`);
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+        const urlencoded = new URLSearchParams();
+        urlencoded.append("siteurl", `${process.env.SITE_URL}`);
+        urlencoded.append("action", "dw");
+        urlencoded.append("firstEntry", "false");
+        urlencoded.append("hashRecordKey", "");
+        urlencoded.append("rcid", `${rcid}`);
+        urlencoded.append("recordID", `${recordID}`);
+        urlencoded.append("password", `${recording.password}`);
+
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: "follow",
+        };
+
+        // Request#2
+        const responseAfterPasswordVerification = await fetch(
+          `${process.env.ORIGIN}/svc3300/svccomponents/servicerecordings/recordingpasswordcheck.do`,
+          requestOptions
+        );
+        const parsedresponseAfterPasswordVerification =
+          await responseAfterPasswordVerification.text();
+        const regexForDownloadUrl = /var\s+href='(.*?)';/;
+
+        const downloadUrl = regexForDownloadUrl.exec(
+          parsedresponseAfterPasswordVerification
+        )[1];
+
+        const downloadUrlObj = new URL(downloadUrl);
+        const recordKey = downloadUrlObj.searchParams.get("recordKey");
+        const recordIDDownload = downloadUrlObj.searchParams.get("recordID");
+        const serviceRecordIDDownload =
+          downloadUrlObj.searchParams.get("serviceRecordID");
+
+        const myHeadersGenrateDownload = new Headers();
+        myHeadersGenrateDownload.append(
+          "User-Agent",
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0"
+        );
+        myHeadersGenrateDownload.append(
+          "Accept",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+        );
+        myHeadersGenrateDownload.append(
+          "Referer",
+          `${process.env.ORIGIN}/svc3300/svccomponents/servicerecordings/recordingpasswordcheck.do`
+        );
+        myHeadersGenrateDownload.append(
+          "Content-Type",
+          "application/x-www-form-urlencoded"
+        );
+        myHeadersGenrateDownload.append("Origin", `${process.env.ORIGIN}`);
+
+        const urlencodedDownload = new URLSearchParams();
+        urlencodedDownload.append("siteurl", `${process.env.SITE_URL}`);
+        urlencodedDownload.append("action", "publishfile");
+        urlencodedDownload.append("recordID", `${recordIDDownload}`);
+        urlencodedDownload.append(
+          "serviceRecordID",
+          `${serviceRecordIDDownload}`
+        );
+        urlencodedDownload.append("recordKey", `${recordKey}`);
+
+        const requestOptionsGenerateDownload = {
+          method: "POST",
+          headers: myHeadersGenrateDownload,
+          body: urlencodedDownload,
+          redirect: "follow",
+        };
+
+        // Request#3
+        const responseDownloadPublish = await fetch(
+          `${process.env.ORIGIN}/mw3300/mywebex/nbrshared.do`,
+          requestOptionsGenerateDownload
+        );
+
+        const parsedResponseDownloadPublish =
+          await responseDownloadPublish.text();
+
+        const downloadUrlRegex = /downloadUrl = '(.*?)'/;
+        const recordIdRegex = /var recordId = (\d+);/;
+        const serviceRecordIdRegex = /var serviceRecordId = (\d+);/;
+        const prepareTicketRegex = /var prepareTicket = '([^']+)';/;
+
+        const downloadUrlMatch =
+          parsedResponseDownloadPublish.match(downloadUrlRegex);
+        const recordIdMatch =
+          parsedResponseDownloadPublish.match(recordIdRegex);
+        const serviceRecordIdMatch =
+          parsedResponseDownloadPublish.match(serviceRecordIdRegex);
+        const prepareTicketMatch =
+          parsedResponseDownloadPublish.match(prepareTicketRegex);
+
+        let recordIdForDownloadUrl;
+        let serviceRecordIdForDownloadUrl;
+        let prepareTicketForDownloadUrl;
+        let baseDownloadUrl;
+        if (recordIdMatch && recordIdMatch[1]) {
+          recordIdForDownloadUrl = recordIdMatch[1];
+        }
+
+        if (serviceRecordIdMatch && serviceRecordIdMatch[1]) {
+          serviceRecordIdForDownloadUrl = serviceRecordIdMatch[1];
+        }
+
+        if (prepareTicketMatch && prepareTicketMatch[1]) {
+          prepareTicketForDownloadUrl = prepareTicketMatch[1];
+        }
+
+        if (downloadUrlMatch && downloadUrlMatch[1]) {
+          baseDownloadUrl = downloadUrlMatch[1];
+        }
+
+        const myHeadersDirectDownload = new Headers();
+        myHeadersDirectDownload.append(
+          "User-Agent",
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0"
+        );
+        myHeadersDirectDownload.append(
+          "Accept",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+        );
+
+        myHeadersDirectDownload.append(
+          "Referer",
+          `${process.env.ORIGIN}/mw3300/mywebex/nbrshared.do`
+        );
+
+        // Request#4
+        let directDownloadResponse = await fetch(
+          `${process.env.ORIGIN}/mw3300/mywebex/nbrPrepare.do?siteurl=${process.env.SITE_URL}&recordid=${recordIdForDownloadUrl}&prepareTicket=${prepareTicketForDownloadUrl}&serviceRecordId=${serviceRecordIdForDownloadUrl}`
+        );
+
+        let parsedDirectDownloadResponse = await directDownloadResponse.text();
+
+        const regexDirectDownloadUrl =
+          /window\.parent\.func_prepare\('([^']*)','([^']*)','([^']*)'/;
+        const directDownloadUrl = parsedDirectDownloadResponse.match(
+          regexDirectDownloadUrl
+        );
+
+        const finalUrl = baseDownloadUrl + directDownloadUrl[3];
+        recordingsWithDirectUrl.push({
+          downloadUrl: finalUrl,
+          name: recording.topic,
+        });
+      } catch (error) {
+        console.error(
+          `Error generating direct download url for ${recording.topic} : `,
+          error
+        );
+      }
+    }
+    console.log("Finished genrerating download urls for recordings");
+    return recordingsWithDirectUrl;
+  } catch (error) {
+    console.error(`Error getting recordings`, error);
+  }
+}
 
 module.exports = {
   genrateDownloadUrl,
